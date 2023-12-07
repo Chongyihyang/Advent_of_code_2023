ae1b6e379bba09ccf39d7ef9c66e8ec1ad6eb4b3
CYY <chongyihyang713@gmail.com>
2023-12-07 19:31:35 +0800

    7th day

---
+++ src/AOC7.rs
@@ -0,0 +1,281 @@
+pub mod aoc7_1 {
+    use std::cmp::Ordering;
+    use std::iter::zip;
+    use crate::{int, range};
+    use crate::python_builtins::builtins::read;
+
+    pub fn rank_hands(hands: &str) -> u8 {
+
+        // check for 5, 4, 3 a kind, or full hse
+        let mut tmp = vec![];
+        for item in hands.chars(){
+            tmp.push(item);
+        }
+        tmp.sort();
+        //println!("{:?}", tmp);
+
+        // full house
+        if (tmp[0] == tmp[2] && tmp[3] == tmp[4] && tmp[0] != tmp[3]) ||
+            (tmp[0] == tmp[1] && tmp[2] == tmp[4] && tmp[0] != tmp[2]){
+            return 5;
+        }
+
+        // 5, 4, 3 a kind
+        let mut counter = 0;
+        let mut first = tmp[0];
+        for &item in tmp.iter(){
+            if item != first && counter < 3 {
+                counter = 1;
+                first = item;
+            } else if item != first && counter >= 3 {
+                break;
+            } else {
+                counter += 1;
+            }
+        }
+        match counter{
+            5 => {return 7},
+            4 => {return 6},
+            3 => {return 4},
+            _ => {}
+        }
+
+        // find pairs
+
+        let mut counter = 0u8;
+        let mut i = 0usize;
+        loop{
+            if i >= 4 {
+                break
+            }
+            if tmp[i] == tmp[i+1]{
+                counter += 1;
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+
+        return match counter {
+            2 => { 3 }
+            1 => { 2 }
+            _ => { 1 }
+        }
+    }
+
+    // if card1 == card2, returns true
+    pub fn card1_is_larger (card1: &str, card2: &str) -> bool{
+        let rank = ['A', 'K', 'Q', 'J', 'T', '9',
+                             '8', '7', '6', '5', '4', '3', '2',];
+        for (i, j) in zip(card1.chars(), card2.chars()){
+            match rank.iter().position(|x| x == &i).unwrap().cmp(&rank.iter().position(|x| x == &j).unwrap()) {
+                Ordering::Less => {return true}
+                Ordering::Equal => {}
+                Ordering::Greater => {return false}
+            }
+        }
+        true
+    }
+
+    pub fn main_7_1() {
+        let contents = read("AOC7.txt");
+        let mut contents: Vec<(&str, u32)>= contents.lines().map(|x| {
+            let tmp: Vec<_> = x.split(' ').collect();
+            (tmp[0], int!(tmp[1], u32))
+            })
+            .collect();
+        contents.sort_by_key(|x| rank_hands(x.0));
+
+        // insertion sort closure
+        let insertion_sort = |mut x: &mut Vec<(&str, u32)>|{
+            for mut i in (1..x.len()){
+                while i > 0 && rank_hands(x[i].0) == rank_hands(x[i-1].0) && card1_is_larger(x[i-1].0, x[i].0) {
+                    x.swap(i, i-1);
+                    i -= 1;
+                }
+            }
+        };
+        insertion_sort(&mut contents);
+
+        // now we give a rank, and add them up
+        let mut total = 0u32;
+        for (rank, &i) in contents.iter().enumerate(){
+            total += (rank as u32+ 1) * i.1
+        }
+
+        println!("{total}");
+    }
+
+}
+#[cfg(test)]
+pub mod tests_aoc7_1 {
+    use crate::AOC7::aoc7_1::rank_hands;
+    use crate::AOC7::aoc7_1::card1_is_larger;
+    #[test]
+    fn rank_hands_works() {
+        assert_eq!(rank_hands("32T3K"), 2);
+        assert_eq!(rank_hands("T55J5"), 4);
+        assert_eq!(rank_hands("KK677"), 3);
+        assert_eq!(rank_hands("KTJJT"), 3);
+        assert_eq!(rank_hands("QQQJA"), 4);
+
+        // given test cases
+        assert_eq!(rank_hands("AAAAA"), 7);
+        assert_eq!(rank_hands("AA8AA"), 6);
+        assert_eq!(rank_hands("23332"), 5);
+        assert_eq!(rank_hands("TTT98"), 4);
+        assert_eq!(rank_hands("23432"), 3);
+        assert_eq!(rank_hands("A23A4"), 2);
+        assert_eq!(rank_hands("23456"), 1);
+    }
+
+    #[test]
+    fn card1_is_larger_works() {
+        assert_eq!(card1_is_larger("KTJJT", "KK677"), false);
+        assert_eq!(card1_is_larger("T55J5", "QQQJA"), false);
+    }
+
+}
+
+
+pub mod aoc7_2 {
+    use std::cmp::Ordering;
+    use std::iter::zip;
+    use crate::AOC7::aoc7_1::rank_hands;
+    use crate::int;
+    use crate::python_builtins::builtins::read;
+
+    pub fn rank_hands_2(hands: &str) -> u8 {
+
+        let mut tmp = vec![];
+        for item in hands.chars(){
+            tmp.push(item);
+        }
+        tmp.sort();
+        if !tmp.contains(&'J') {
+            return rank_hands(hands)
+        }
+        return match tmp.iter().filter(|&x| x == &'J').count() {
+            5 => { 7 }
+            4 => { 7 }
+            3 => {
+                let rank = rank_hands(hands);
+                if rank == 5 {
+                    // there will be 3 x cards and 2 y cards
+                    return 7
+                }
+                if rank == 4 {
+                    // there will be 3 x cards, 1 y card and 1 z card
+                    return 6
+                }
+                7
+            }
+            2 => {
+                let rank = rank_hands(hands);
+                if rank == 5 {
+                    // there will be 3 x cards and 2 y cards
+                    return 7
+                }
+                if rank == 3 {
+                    // there will be 2 x cards, 2 y card and 1 z card. W L O G, let 2 x be the 'J'
+                    return 6
+                }
+                if rank == 2 {
+                    // there will be 2 x cards, 1 y card, 1 z card, and 1 t card. W L O G, let 2 x be 'J'
+                    return 4
+                }
+                0
+            }
+            1 => {
+                let rank = rank_hands(hands);
+                if rank == 6 {
+                    return 7
+                }
+                if rank == 4 {
+                    // there will be 3 x cards, 1 y card and 1 z card
+                    return 6
+                }
+                if rank == 3 {
+                    // there will be 2 x cards, 2 y card and 1 z card. W L O G, let 1 z be the 'J'
+                    return 5
+                }
+                if rank == 2 {
+                    // there will be 2 x cards, 1 y card, 1 z card, and 1 t card. W L O G, let 1 t be 'J'
+                    return 4
+                }
+                2
+            }
+            _ => {1}
+        }
+    }
+
+    // if card1 == card2, returns true
+    pub fn card1_is_larger_2 (card1: &str, card2: &str) -> bool{
+        let rank = ['A', 'K', 'Q', 'T', '9',
+            '8', '7', '6', '5', '4', '3', '2', 'J'];
+        for (i, j) in zip(card1.chars(), card2.chars()){
+            match rank.iter().position(|x| x == &i).unwrap().cmp(&rank.iter().position(|x| x == &j).unwrap()) {
+                Ordering::Less => {return true}
+                Ordering::Equal => {}
+                Ordering::Greater => {return false}
+            }
+        }
+        true
+    }
+
+    pub fn main_7_2() {
+        let contents = read("AOC7.txt");
+        let mut contents: Vec<(&str, u32)>= contents.lines().map(|x| {
+            let tmp: Vec<_> = x.split(' ').collect();
+            (tmp[0], int!(tmp[1], u32))
+        })
+            .collect();
+        contents.sort_by_key(|x| rank_hands_2(x.0));
+
+        // insertion sort closure
+        let insertion_sort = |mut x: &mut Vec<(&str, u32)>|{
+            for mut i in (1..x.len()){
+                while i > 0 && rank_hands_2(x[i].0) == rank_hands_2(x[i-1].0) && card1_is_larger_2(x[i-1].0, x[i].0) {
+                    x.swap(i, i-1);
+                    i -= 1;
+                }
+            }
+        };
+        insertion_sort(&mut contents);
+
+        // now we give a rank, and add them up
+        let mut total = 0u32;
+        for (rank, &i) in contents.iter().enumerate(){
+            total += (rank as u32+ 1) * i.1
+        }
+
+        println!("{total}");
+    }
+
+}
+#[cfg(test)]
+pub mod tests_aoc7_2 {
+    use crate::AOC7::aoc7_2::{card1_is_larger_2, rank_hands_2};
+
+    #[test]
+    fn rank_hands_works() {
+        assert_eq!(rank_hands_2("32T3K"), 2);
+        assert_eq!(rank_hands_2("T55J5"), 6);
+        assert_eq!(rank_hands_2("KK677"), 3);
+        assert_eq!(rank_hands_2("KTJJT"), 6);
+        assert_eq!(rank_hands_2("QQQJA"), 6);
+
+        // given test cases
+        assert_eq!(rank_hands_2("8JJA9"), 4);
+        assert_eq!(rank_hands_2("AA8AA"), 6);
+        assert_eq!(rank_hands_2("23332"), 5);
+        assert_eq!(rank_hands_2("TTT98"), 4);
+        assert_eq!(rank_hands_2("23432"), 3);
+        assert_eq!(rank_hands_2("A23A4"), 2);
+        assert_eq!(rank_hands_2("23456"), 1);
+        assert_eq!(rank_hands_2("JTTJT"), 7);
+        assert_eq!(rank_hands_2("J2J28"), 6);
+        assert_eq!(card1_is_larger_2("QQQJA","J2J28"), true);
+    }
+
+}
---
+++ src/python_builtins.rs
@@ -0,0 +1,131 @@
+pub mod builtins{
+    use std::cmp::Ordering;
+    use std::fmt::Display;
+    use std::fs::{File, OpenOptions, remove_file};
+    use std::io::{Read, Write};
+
+    #[macro_export]
+    macro_rules! range{
+        ($x: expr) => {
+            (0..$x)
+        };
+        ($x: expr, $y: expr) => {
+            ($x..$y)
+        };
+        ($x: expr, $y: expr, $z: expr) => {
+            ($x..$y).step_by($z as usize)
+        };
+        ($x: expr, $y: expr, $z: expr, $sign: expr ) => {
+            ($x..$y).step_by($z as usize).rev()
+        }
+    }
+
+    #[macro_export]
+    macro_rules! input {
+        () => {{
+            print!("\n");
+            let mut new = String::new();
+            std::io::stdin().read_line(&mut new).unwrap();
+            new.trim().to_string()
+        }};
+        ($($arg:tt)*) => {{
+            println!("{}", ($($arg)*));
+            let mut new = String::new();
+            std::io::stdin().read_line(&mut new).unwrap();
+            new.trim().to_string()
+        }};
+    }
+
+    pub fn write(filename: &str, content: &str, modes: &str){
+        match modes{
+            "w" => {
+                let mut file;
+                match File::open(filename){
+                    Ok(..) => {
+                        remove_file(filename).expect("could not remove file");
+                        file = File::create(filename).unwrap();
+                    },
+                    Err(..) => {
+                        file = File::create(filename).unwrap();
+                    }
+                }
+                file.write(content.as_bytes())
+                    .expect("write failed");
+            },
+            "a" => {
+                let mut file = OpenOptions::new()
+                    .append(true)
+                    .open(filename)
+                    .expect("cannot open file");
+
+                file.write(content.as_bytes())
+                    .expect("write failed");
+            },
+            _ => {}
+        }
+    }
+
+    pub fn read(filename: &str) -> String {
+        let mut file = File::open(filename).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        contents
+    }
+
+
+    #[macro_export]
+    macro_rules! len {
+        ($x: expr) => {
+            $x.len()
+        };
+    }
+
+    #[macro_export]
+    macro_rules! int {
+        ($x: expr) => {
+            $x.parse::<i64>().unwrap()
+        };
+        ($x: expr, $y: ident) => {
+            $x.parse::<$y>().unwrap()
+        };
+    }
+
+    #[macro_export]
+    macro_rules! str {
+        () => {
+            String::new()
+        };
+        ($x: expr) => {
+            $x.to_string()
+        };
+    }
+
+    pub fn ord(some_chr: char) -> u32{
+        let something = some_chr.clone();
+        something as u32
+    }
+
+    pub fn chr(some_num: &u8) -> char {
+        let something = some_num.clone();
+        something as char
+    }
+
+    pub fn search_arr<T: PartialOrd>(lst: &[T], item: &T) -> isize{
+        let mut l = 0 as usize;
+        let mut r = len!(lst) - 1;
+        while l <= r {
+            let mid = (l + r) / 2;
+            match lst[mid].partial_cmp(item) {
+                Some(Ordering::Greater) => r = mid - 1,
+                Some(Ordering::Equal) => return mid as isize,
+                Some(Ordering::Less) => l = mid + 1,
+                _ => {}
+            }
+        }
+        -1
+    }
+
+    pub fn clear_line(){
+        print!("\x1B[2J\x1B[1;1H");
+    }
+}
