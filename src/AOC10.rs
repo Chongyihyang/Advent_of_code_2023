8109fd41537061d4b1c30be6d0fea95651759031
CYY <chongyihyang713@gmail.com>
2023-12-11 12:51:36 +0800

    day 10

--- src/AOC10.rs
+++ src/AOC10.rs
@@ -1,39 +1,366 @@
 pub mod aoc10_2 {
-    use crate::python_builtins::builtins::{chr, ord, read, write};
+    use std::collections::VecDeque;
+    use std::str::FromStr;
+    use crate::python_builtins::builtins::read;
 
+    const GRID_SIZE: usize = 140;
 
-
-    pub fn main10_2(){
-        let contents = read("AOC10_2.txt");
-        let mut contents: Vec<_> = contents
-            .lines()
-            .map(|x| {
-                x.chars().map(|mut x| {
-                    if x == '.'{
-                        x = '0'
-                    } else if ord(x) >= 25000 {
-                        x = ' '
-                    } else {
-                        x = '*'
-                    }
-                    x
-                }).collect::<Vec<char>>()
-            })
-            .collect();
-        let mut contents = contents
-            .iter()
-            .map(|x| {
-                let mut x = x.iter().collect::<String>();
-                x.push_str("\r\n");
-                //println!("{x}");
-                x
-            })
-            .collect::<String>();
-        write("AOC10_2.txt", &*contents, "w");
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    enum Direction {
+        North,
+        East,
+        South,
+        West,
     }
 
+    const COMPASS: [Direction; 4] = [
+        Direction::North,
+        Direction::East,
+        Direction::South,
+        Direction::West,
+    ];
 
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    struct Pipe(Direction, Direction);
 
+    impl Pipe {
+        fn traverse(self, edge: Direction) -> Option<Direction> {
+            if edge == self.0 {
+                Some(self.1)
+            } else if edge == self.1 {
+                Some(self.0)
+            } else {
+                None
+            }
+        }
+
+        fn read_from_char(ch: char) -> Option<Self> {
+            match ch {
+                '|' => Some(Self(Direction::North, Direction::South)),
+                '-' => Some(Self(Direction::East, Direction::West)),
+                'L' => Some(Self(Direction::North, Direction::East)),
+                'J' => Some(Self(Direction::North, Direction::West)),
+                '7' => Some(Self(Direction::South, Direction::West)),
+                'F' => Some(Self(Direction::East, Direction::South)),
+                _ => None,
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct EdgePosition {
+        pos: usize,
+        edge: Direction,
+        steps: u32,
+    }
+
+    impl EdgePosition {
+        fn cross_edge(&self) -> Self {
+            match self.edge {
+                Direction::North => Self {
+                    pos: self.pos - GRID_SIZE,
+                    edge: Direction::South,
+                    steps: self.steps,
+                },
+                Direction::East => Self {
+                    pos: self.pos + 1,
+                    edge: Direction::West,
+                    steps: self.steps,
+                },
+                Direction::South => Self {
+                    pos: self.pos + GRID_SIZE,
+                    edge: Direction::North,
+                    steps: self.steps,
+                },
+                Direction::West => Self {
+                    pos: self.pos - 1,
+                    edge: Direction::East,
+                    steps: self.steps,
+                },
+            }
+        }
+
+        fn traverse_pipe(&self, pipe: Pipe) -> Option<Self> {
+            pipe.traverse(self.edge).map(|edge| Self {
+                pos: self.pos,
+                edge,
+                steps: self.steps + 1,
+            })
+        }
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq)]
+    enum Corner {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+    }
+
+    impl Corner {
+        fn move_crosses(self, direction: Direction) -> Option<Direction> {
+            match (self, direction) {
+                (Corner::TopLeft, Direction::East) | (Corner::TopRight, Direction::West) => {
+                    Some(Direction::North)
+                }
+                (Corner::TopLeft, Direction::South) | (Corner::BottomLeft, Direction::North) => {
+                    Some(Direction::West)
+                }
+                (Corner::TopRight, Direction::South) | (Corner::BottomRight, Direction::North) => {
+                    Some(Direction::East)
+                }
+                (Corner::BottomLeft, Direction::East) | (Corner::BottomRight, Direction::West) => {
+                    Some(Direction::South)
+                }
+                _ => None,
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct CornerPosition {
+        pos: usize,
+        corner: Corner,
+    }
+
+    impl CornerPosition {
+        fn adjacent_positions<'a>(&'a self, pipe: &'a Option<Pipe>) -> impl Iterator<Item = Self> + 'a {
+            // calculate adjacent locations, accounting for OOB
+            let north = self.pos.checked_sub(GRID_SIZE);
+            let east = if (self.pos % GRID_SIZE) == (GRID_SIZE - 1) {
+                None
+            } else {
+                Some(self.pos + 1)
+            };
+            let west = self.pos.checked_sub(1);
+            let south = if (self.pos / GRID_SIZE) == (GRID_SIZE - 1) {
+                None
+            } else {
+                Some(self.pos + GRID_SIZE)
+            };
+
+            COMPASS.iter().filter_map(move |direction| {
+                // check for move blocked by part of pipe
+                if let Some(cross) = self.corner.move_crosses(*direction) {
+                    if let Some(pipe) = pipe {
+                        if pipe.0 == cross || pipe.1 == cross {
+                            return None;
+                        }
+                    }
+                }
+
+                // calculate destination
+                match (direction, self.corner) {
+                    (Direction::North, Corner::TopLeft) => north.map(|pos| Self {
+                        pos,
+                        corner: Corner::BottomLeft,
+                    }),
+                    (Direction::North, Corner::TopRight) => north.map(|pos| Self {
+                        pos,
+                        corner: Corner::BottomRight,
+                    }),
+                    (Direction::North, Corner::BottomLeft) | (Direction::West, Corner::TopRight) => {
+                        Some(Self {
+                            pos: self.pos,
+                            corner: Corner::TopLeft,
+                        })
+                    }
+                    (Direction::North, Corner::BottomRight) | (Direction::East, Corner::TopLeft) => {
+                        Some(Self {
+                            pos: self.pos,
+                            corner: Corner::TopRight,
+                        })
+                    }
+                    (Direction::East, Corner::TopRight) => east.map(|pos| Self {
+                        pos,
+                        corner: Corner::TopLeft,
+                    }),
+                    (Direction::East, Corner::BottomLeft) | (Direction::South, Corner::TopRight) => {
+                        Some(Self {
+                            pos: self.pos,
+                            corner: Corner::BottomRight,
+                        })
+                    }
+                    (Direction::East, Corner::BottomRight) => east.map(|pos| Self {
+                        pos,
+                        corner: Corner::BottomLeft,
+                    }),
+                    (Direction::South, Corner::TopLeft) | (Direction::West, Corner::BottomRight) => {
+                        Some(Self {
+                            pos: self.pos,
+                            corner: Corner::BottomLeft,
+                        })
+                    }
+                    (Direction::South, Corner::BottomLeft) => south.map(|pos| Self {
+                        pos,
+                        corner: Corner::TopLeft,
+                    }),
+                    (Direction::South, Corner::BottomRight) => south.map(|pos| Self {
+                        pos,
+                        corner: Corner::TopRight,
+                    }),
+                    (Direction::West, Corner::TopLeft) => west.map(|pos| Self {
+                        pos,
+                        corner: Corner::TopRight,
+                    }),
+                    (Direction::West, Corner::BottomLeft) => west.map(|pos| Self {
+                        pos,
+                        corner: Corner::BottomRight,
+                    }),
+                }
+            })
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct CornerVisitTracker([u8; GRID_SIZE * GRID_SIZE]);
+
+    impl CornerVisitTracker {
+        fn new() -> Self {
+            Self([0; GRID_SIZE * GRID_SIZE])
+        }
+
+        fn count_unvisited(&self) -> u32 {
+            self.0
+                .iter()
+                .fold(0, |empty, val| empty + u32::from(val == &0))
+        }
+
+        fn visit(&mut self, pos: &CornerPosition) -> bool {
+            let value = match pos.corner {
+                Corner::TopLeft => 1,
+                Corner::TopRight => 2,
+                Corner::BottomLeft => 4,
+                Corner::BottomRight => 8,
+            };
+            let visited = self.0[pos.pos] & value == value;
+            self.0[pos.pos] |= value;
+            visited
+        }
+    }
+
+    struct Maze {
+        start: usize,
+        grid: [Option<Pipe>; GRID_SIZE * GRID_SIZE],
+    }
+
+    impl Maze {
+        fn furthest_point_in_loop(&self) -> Option<u32> {
+            let mut visited = [false; GRID_SIZE * GRID_SIZE];
+            let mut queue = VecDeque::new();
+
+            for edge in COMPASS {
+                queue.push_back(
+                    EdgePosition {
+                        pos: self.start,
+                        edge,
+                        steps: 0,
+                    }
+                        .cross_edge(),
+                );
+            }
+
+            while let Some(pos) = queue.pop_front() {
+                if let Some(pipe) = self.grid[pos.pos] {
+                    if let Some(next) = pos.traverse_pipe(pipe).map(|p| p.cross_edge()) {
+                        if visited[next.pos] {
+                            return Some(next.steps + 1);
+                        }
+                        visited[next.pos] = true;
+                        queue.push_back(next);
+                    }
+                }
+            }
+
+            None
+        }
+
+        fn replacement_start_pipe(&self) -> Option<Pipe> {
+            let pos = self.start;
+            let mut edges = COMPASS.iter().filter_map(|edge| {
+                let cross = EdgePosition {
+                    pos,
+                    edge: *edge,
+                    steps: 0,
+                }
+                    .cross_edge();
+                let Some(pipe) = self.grid[cross.pos] else {
+                    return None;
+                };
+                cross.traverse_pipe(pipe).map(|_| edge)
+            });
+
+            let first = edges.next();
+            let second = edges.next();
+            match (first, second) {
+                (Some(first), Some(second)) => Some(Pipe(*first, *second)),
+                _ => None,
+            }
+        }
+
+        fn spaces_enclosed_by_loop(&self) -> u32 {
+            let mut visited = CornerVisitTracker::new();
+            let mut queue = VecDeque::new();
+
+            let start_pipe = self.replacement_start_pipe();
+
+            queue.push_back(CornerPosition {
+                pos: 0,
+                corner: Corner::TopLeft,
+            });
+
+            while let Some(pos) = queue.pop_front() {
+                if !visited.visit(&pos) {
+                    let pipe = if pos.pos == self.start {
+                        start_pipe
+                    } else {
+                        self.grid[pos.pos]
+                    };
+                    queue.extend(pos.adjacent_positions(&pipe));
+                }
+            }
+
+            visited.count_unvisited()
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct ParseMazeError;
+
+    impl FromStr for Maze {
+        type Err = ParseMazeError;
+
+        fn from_str(input: &str) -> Result<Self, Self::Err> {
+            let mut start = 0;
+            let mut grid = [None; GRID_SIZE * GRID_SIZE];
+
+            for (row, line) in input.lines().enumerate() {
+                let row_start = row * GRID_SIZE;
+                for (col, ch) in line.chars().enumerate() {
+                    let pos = row_start + col;
+                    grid[pos] = Pipe::read_from_char(ch);
+                    if ch == 'S' {
+                        start = pos;
+                    }
+                }
+            }
+
+            Ok(Self { start, grid })
+        }
+    }
+
+    #[must_use]
+    pub fn main10_2(){
+        let input = read("AOC10.txt");
+        let input = &*input;
+        if let Ok(maze) = Maze::from_str(input) {
+            println!("{}", maze.spaces_enclosed_by_loop());
+        }
+    }
+}
+
+pub mod aoc10_1 {
+    use crate::python_builtins::builtins::{chr, ord, read};
     pub fn change_char(input: String, x: u32, y: u32, count: u32) -> String {
         // let og = input.len();
         let (x, y) = (x as usize, y as usize);
@@ -69,13 +396,13 @@ pub mod aoc10_2 {
 
     #[derive(Clone)]
     pub struct Frame {
-        top: Option<char>,
-        bottom: Option<char>,
-        left: Option<char>,
-        right: Option<char>,
-        coord: (u32, u32),
+        pub top: Option<char>,
+        pub bottom: Option<char>,
+        pub left: Option<char>,
+        pub right: Option<char>,
+        pub coord: (u32, u32),
         pub count: u32,
-        character: char,
+        pub character: char,
     }
 
     impl Frame {
@@ -88,7 +415,7 @@ pub mod aoc10_2 {
 
             // settle the top
             let mut top = None;
-            if y > 0{
+            if y >= 1{
                 let curr = input.lines().nth(y - 1);
                 if let Some(i) = curr {
                     top = curr.unwrap().chars().nth(x);
@@ -119,32 +446,32 @@ pub mod aoc10_2 {
         pub fn change_tabs(&self, mut input: String) -> (Vec<Frame>, String) {
             let mut oth = vec![];
             let (x, y) = self.coord;
-            input = change_char(input.clone(), x, y, self.count);
+            input = change_char(input.clone(), x, y, 48);
             let (x , y) = (x as usize, y as usize);
             match self.character{
                 '|' => {
-                    if ord(get_char(&input, x, y-1)) < 25000 {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
-                    if ord(get_char(&input, x, y+1)) < 25000 {oth.push(Frame::new(input.clone(), x, y + 1,self.count + 1, self.bottom.unwrap()))};
+                    if get_char(&input, x, y-1) != '0' {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
+                    if get_char(&input, x, y+1) != '0' {oth.push(Frame::new(input.clone(), x, y + 1, self.count + 1, self.bottom.unwrap()))};
                 },
                 '-' => {
-                    if ord(get_char(&input, x-1, y)) < 25000 {oth.push(Frame::new(input.clone(), x - 1, y, self.count + 1, self.left.unwrap()))};
-                    if ord(get_char(&input, x+1, y)) < 25000 {oth.push(Frame::new(input.clone(), x + 1, y,self.count + 1, self.right.unwrap()))};
+                    if get_char(&input, x-1, y) != '0' {oth.push(Frame::new(input.clone(), x - 1, y, self.count + 1, self.left.unwrap()))};
+                    if get_char(&input, x+1, y) != '0' {oth.push(Frame::new(input.clone(), x + 1, y, self.count + 1, self.right.unwrap()))};
                 },
                 'L' => {
-                    if ord(get_char(&input, x, y-1)) < 25000 {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
-                    if ord(get_char(&input, x+1, y)) < 1000 {oth.push(Frame::new(input.clone(), x + 1, y,self.count + 1, self.right.unwrap()))};
+                    if get_char(&input, x, y-1) != '0' {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
+                    if get_char(&input, x+1, y) != '0' {oth.push(Frame::new(input.clone(), x + 1, y, self.count + 1, self.right.unwrap()))};
                 },
                 'J' => {
-                    if ord(get_char(&input, x, y-1)) < 25000 {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
-                    if ord(get_char(&input, x-1, y)) < 1000 {oth.push(Frame::new(input.clone(), x - 1, y,self.count + 1, self.left.unwrap()))};
+                    if get_char(&input, x, y-1) !='0' {oth.push(Frame::new(input.clone(), x, y - 1, self.count + 1, self.top.unwrap()))};
+                    if get_char(&input, x-1, y) !='0' {oth.push(Frame::new(input.clone(), x - 1, y, self.count + 1, self.left.unwrap()))};
                 },
-                '+' => {
-                    if ord(get_char(&input, x-1, y)) < 25000 {oth.push(Frame::new(input.clone(), x - 1, y, self.count + 1, self.left.unwrap()))};
-                    if ord(get_char(&input, x, y+1)) < 1000 {oth.push(Frame::new(input.clone(), x, y + 1,self.count + 1, self.bottom.unwrap()))};
+                '7' => {
+                    if get_char(&input, x-1, y) != '0' {oth.push(Frame::new(input.clone(), x - 1, y, self.count + 1, self.left.unwrap()))};
+                    if get_char(&input, x, y+1) != '0' {oth.push(Frame::new(input.clone(), x, y + 1, self.count + 1, self.bottom.unwrap()))};
                 },
                 'F' => {
-                    if ord(get_char(&input, x+1, y)) < 25000 {oth.push(Frame::new(input.clone(), x + 1, y, self.count + 1, self.right.unwrap()))};
-                    if ord(get_char(&input, x, y+1)) < 25000 {oth.push(Frame::new(input.clone(), x, y + 1,self.count + 1, self.bottom.unwrap()))};
+                    if get_char(&input, x+1, y) != '0' {oth.push(Frame::new(input.clone(), x + 1, y, self.count + 1, self.right.unwrap()))};
+                    if get_char(&input, x, y+1) != '0' {oth.push(Frame::new(input.clone(), x, y + 1, self.count + 1, self.bottom.unwrap()))};
                 }
                 _ => {}
             }
@@ -155,21 +482,13 @@ pub mod aoc10_2 {
             self.coord
         }
     }
-}
-
-pub mod aoc10_1 {
-    use crate::python_builtins::builtins::read;
-    use crate::AOC10::aoc10_2::Frame;
 
     pub fn main10_1() {
         let mut contents = read("AOC10.txt");
-        // current character = 'L'
         let mut frame = Frame::new(contents.clone(), 74, 95, 25000, 'L');
         let mut queue = vec![];
         let mut high = 0u32;
         loop {
-            // let (x, y) = frame.coord();
-            // println!("{x}, {y}");
             let tmp = frame.change_tabs(contents.clone());
             let mut new_frames = tmp.0;
             contents = tmp.1;
@@ -181,7 +500,6 @@ pub mod aoc10_1 {
             if queue.len() == 0 {
                 break;
             }
-            //println!("{contents}");
         }
         println!("{}", high - 25000);
         println!("{contents}");
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-use crate::AOC10::aoc10_1::main10_1;
 use crate::AOC10::aoc10_2::main10_2;
-
-mod AOC1;
-mod AOC10;
-mod AOC2;
-mod AOC9;
 mod python_builtins;
-
+mod AOC10;
 fn main() {
-    use std::time::Instant;
-    let now = Instant::now();
-
-    // Code block to measure.
-    {
-        main10_2();
-    }
-
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    main10_2();
 }
